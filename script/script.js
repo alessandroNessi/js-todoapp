98fc9faf25463a8f2fff92a50b5c79c80e9a3dc2
stormbringer88 <alessandronessi@msn.com>
2021-08-10 23:32:04 +0200

    added populatedtable with deletable elements

--- script/script.js
+++ script/script.js
@@ -1,15 +1,37 @@
-var listPopulation = ['Complete online javascript course','Jog around the park 3x','10 minutes meditation', 'Read for 1 hour', 'Pick up groceries'];
-var listPopulationStatus = [1 , 0, 0, 0, 0, 0];
+var listPopulation = ['Complete online javascript course','Jog around the park 3x','10 minutes meditation', 'Read for 1 hour', 'Pick up groceries','Do the locomotion'];
+var listPopulationStatus = ["checked" , "", "", "", "", "", ""];
+var list = document.getElementById('itemList')
 window.onload = populateTable();
 function populateTable(){
+    //gives height of main to ul
+    var listHeight = document.getElementsByTagName("main")[0].clientHeight;
+    document.getElementById("itemList").style.height=listHeight+"px";
     for(var i=0; i<listPopulation.length; i++){
-        document.getElementsByClassName("container__checkbox--text")[i].innerHTML=listPopulation[i];
-        if(listPopulationStatus[i]==1){
-            document.getElementById("checkbox"+(i+1)).setAttribute("checked", "checked");
-        }else{
-            document.getElementById("checkbox"+(i+1)).checked = false;
-        }
+        alert(i);
+        var node = document.createElement("li");
+        node.insertAdjacentHTML('afterbegin', '<div class="d-flex justify-content-between list__item">'+
+        '<label class="container__checkbox d-flex align-items-center position-relative" for="checkbox'+(i+1)+'">'+'<input type="checkbox" id="checkbox'+(i+1)+'" name="checkbox'+(i+1)+'" value="'+(i+1)+'" '+ listPopulationStatus[i] +'>'+'<span class="container__checkbox--text pe-3 ps-3">'+ listPopulation[i] +'</span></label>'+'<button class="button__delete" onclick="deleteListItem(\'1\')"></button>'+
+    '</div>');
+        alert(listHeight);
+        node.setAttribute("style","height: "+(listHeight/5)+"px;");
+        node.draggable=true;
+        // node.innerHTML=listPopulation[i];
+        list.appendChild(node);
     }
+}
+const compare = (e) =>{
+    var index1 = indexOf(dragging);
+    var index2 = indexOf(draggedOver);
+    splice(index1, 1);
+    splice(index2, 0, dragging);
+}
+const setDraggedOver = (e) => {
+    e.preventDefault();
+    draggedOver = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
+}
+    
+const setDragging = (e) =>{
+    dragging = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
 }
 function changeDayNight(){
     if(document.getElementById("moonIcon").classList.contains("d-none")){
@@ -23,9 +45,8 @@ function changeDayNight(){
     }
 }
 function deleteListItem(idToDelete){
-    // listPopulation.splice(idToDelete-1, 1);
-    // listPopulationStatus.splice(idToDelete-1, 1);
-    listPopulation.push("");
-    listPopulationStatus.push(0);
-    populateTable();
+    var elem = document.getElementsByTagName("li")[idToDelete-1];
+    elem.parentNode.removeChild(elem);
+    listPopulation.splice(idToDelete-1, 1);
+    listPopulationStatus.splice(idToDelete-1, 1);
 }
