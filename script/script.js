e93a323f4e0ab6ee6db3cd6cb0cb93eb2586fb93
stormbringer88 <alessandronessi@msn.com>
2021-08-13 17:07:38 +0200

    complete drag and drop add etc.. functions

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ var listPopulationStatus = ["checked" , "", "", "", "", ""];
 var listPopulationId = [1,2,3,4,5,6]; 
 var list = document.getElementById('itemList');
 var listHeight = document.getElementsByTagName("main")[0].clientHeight;
+var dragId, dropId, dragSrcEl;
+var thisId = false; //i set thisid false when is not in drag and drop so if it's a drag and drop operation the changeid function won't change the index of checked
+let items = document.querySelectorAll('li');
 //gives height to ul
 document.getElementById("itemList").style.height=listHeight+"px";
 window.onload = populateTable();
@@ -15,27 +18,74 @@ function populateTable(){
 function addListElement(i){
     //add an element with checkbox id =i+1
     var node = document.createElement("li");
+    // node.style.add(ondrop="drop(event)");
+    // node.addEventListener("ondrop",ondrop,false);
+    // ondragover=\"allowDrop(event)\"");
         node.insertAdjacentHTML('afterbegin', '<div class="d-flex justify-content-between list__item">'+
-        '<label class="container__checkbox d-flex align-items-center position-relative" for="checkbox'+(i+1)+'">'+'<input type="checkbox" id="checkbox'+(i+1)+'" name="checkbox'+(i+1)+'" onchange = changePopulationStatus("'+(i+1)+'") value="'+(i+1)+'" '+ listPopulationStatus[i] +'>'+'<span class="container__checkbox--text pe-3 ps-3">'+ listPopulation[i] +'</span></label>'+'<button class="button__delete" onclick="deleteListItem(\''+(i+1)+'\')"></button>'+
+        '<label class="container__checkbox d-flex align-items-center position-relative" for="checkbox'+(listPopulationId[i])+'">'+'<input type="checkbox" id="checkbox'+(listPopulationId[i])+'" name="checkbox'+(listPopulationId[i])+'" onchange = changePopulationStatus("'+(listPopulationId[i])+'") value="'+(listPopulationId[i])+'" '+ listPopulationStatus[i] +'>'+'<span class="container__checkbox--text pe-3 ps-3">'+ listPopulation[i] +'</span></label>'+'<button class="button__delete" onclick="deleteListItem(\''+(listPopulationId[i])+'\')"></button>'+
     '</div>');
         node.setAttribute("style","height: "+(listHeight/5)+"px;");
         node.draggable=true;
+        node.addEventListener('dragstart', handleDragStart, false);
+        node.addEventListener('dragend', handleDragEnd, false);
+        node.addEventListener('dragover', handleDragOver, false);
+        node.addEventListener('dragenter', handleDragEnter, false);
+        node.addEventListener('dragleave', handleDragLeave, false);
+        node.addEventListener('drop', dragDrop, false);
+        node.id= listPopulationId[i];
         list.appendChild(node);
 }
-// const compare = (e) =>{
-//     var index1 = indexOf(dragging);
-//     var index2 = indexOf(draggedOver);
-//     splice(index1, 1);
-//     splice(index2, 0, dragging);
-// }
-// const setDraggedOver = (e) => {
-//     e.preventDefault();
-//     draggedOver = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
-// }
-    
-// const setDragging = (e) =>{
-//     dragging = Number.isNaN(parseInt(e.target.innerText)) ? e.target.innerText : parseInt(e.target.innerText)
-// }
+function handleDragStart(e) {
+    this.style.opacity = '0.33';
+    dragId = this.id;
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragEnd(e) {
+    this.style.opacity = '1';
+    // items.forEach(element => element.classList.remove('over');
+    // });
+}
+function handleDragOver(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    return false;
+}
+function handleDragEnter(e) {
+    this.classList.add('over');
+}
+function handleDragLeave(e) {
+    this.classList.remove('over');
+}
+function dragDrop(e) {
+    e.stopPropagation();
+    if (dragSrcEl !== this) {
+        var temp="";
+        thisId = getIndexId(this.id);
+        dragId = getIndexId(dragId);
+        console.log("changing" + dragId +" with " + thisId);
+        //nope
+        temp= listPopulation[thisId];
+        listPopulation[thisId]=listPopulation[dragId];
+        listPopulation[dragId]=temp;
+        temp= listPopulationStatus[thisId];
+        listPopulationStatus[thisId]=listPopulationStatus[dragId];
+        listPopulationStatus[dragId]=temp;
+        temp= listPopulationId[thisId];
+        document.getElementById(listPopulationId[dragId]).id=listPopulationId[thisId];
+        this.id=listPopulationId[dragId];
+        listPopulationId[thisId]=listPopulationId[dragId];
+        listPopulationId[dragId]=temp;
+        dragSrcEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+        dragId="null";
+        thisId=false; //i set thisid false when is not in drag and drop so if it's a drag and drop operation the changeid function won't change the index of checked
+    }
+    return false;
+}
 function changeDayNight(){
     if(document.getElementById("moonIcon").classList.contains("d-none")){
         document.getElementById("moonIcon").classList.remove("d-none");
@@ -60,18 +110,22 @@ function deleteListItem(idToDelete){
 function getIndexId(id){
     for(var i=0; i<listPopulationId.length;i++){
         if(id==listPopulationId[i]){
+            console.log(i);
             return i;
         }
     }
 }
 //given the id number (es 1 for checkbox 1) get the id of the element and change both the status and the value in the listPopulationStatus where the selected id is
 function changePopulationStatus(idToChange){
-    var element = document.getElementById("checkbox"+idToChange).checked;
-    var elementIndex = getIndexId(idToChange);
-    if(element==true){
-        listPopulationStatus[elementIndex]="checked";
-    }else{
-        listPopulationStatus[elementIndex]="";
+    //i set thisid false when is not in drag and drop so if it's a drag and drop operation it won't change the index of checked
+    if(thisId == false){
+        var element = document.getElementById("checkbox"+idToChange).checked;
+        var elementIndex = getIndexId(idToChange);
+        if(element==true){
+            listPopulationStatus[elementIndex]="checked";
+        }else{
+            listPopulationStatus[elementIndex]="";
+        }
     }
 }
 //show all the elements
@@ -108,9 +162,11 @@ function showCompleted(){
 }
 //search for elements with "completed" value inside the listpopulationstatus and gives the delete command to it
 function deleteCompleted(){
-    for(i=0; i<document.getElementsByTagName("main")[0].getElementsByTagName("li").length; i++){
+    for(var i=0; i<document.getElementsByTagName("main")[0].getElementsByTagName("li").length; i++){
         if(listPopulationStatus[i]=="checked"){
             deleteListItem(listPopulationId[i]);
+            //i gives i a position back if i don't it will jump the next position because the array is  position shorter
+            i--;
         }
     }
     document.getElementById("showCompletedButton").classList.remove("d-none");
@@ -161,3 +217,4 @@ function include(array, value){
     }
     return isIcluded;
 }
+//drag
